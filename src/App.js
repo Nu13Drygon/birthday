446d822f9fd05d2b28bc8d3b81dc09c77d9ce957
Wrath <nu13drygon@gmail.com>
2022-09-29 11:29:33 -0500

    fix styling and bugs

--- src/App.js
+++ src/App.js
@@ -4,20 +4,32 @@ import List from "./List";
 
 function App() {
   const [people, setPeople] = useState(data)
+  const [birthdays, setBirthdays] = useState(people.length)
+
+  if(people.length === 0) {
+    return (
+      <div className="text-center">
+        <h1>Removed All Users</h1>
+        <button className="btn btn-outline-primary" onClick={() => setPeople(data)}>Repopulate</button>
+      </div>
+    )
+  }
   
 
   return (
-    <div className="text-center">
-      <header className="">
-        <p className="fs-1">{people.length} birthdays today</p>
-        <div className="container">
+    <>
+      <header className="text-center">
+        <h1>{people.length} birthdays today</h1>
+      </header>
+      <div>
+        <div className="container text-center">
           <div className="row">
             <List people={people} />
           </div>
+          <button type="button" className="btn btn-primary" onClick={() => setPeople([])}>Clear All</button>
         </div>
-      </header>
-      <button type="button" className="btn btn-outline-primary" onClick={() => setPeople([])}>Clear All</button>
-    </div>
+      </div>
+    </>
   );
 }
 
--- src/List.js
+++ src/List.js
@@ -1,18 +1,30 @@
+import { useState } from 'react'
 import './list.css'
 
 const List = ({people}) => {
+    // grabs state from parent component
+    const [users, setUsers] = useState(people)
+
+    //filter users based on id
+    const removeUser = (id) => {
+        let newUsers = users.filter((user) => user.id !== id)
+        setUsers(newUsers)
+        
+    }
+
     return (
         <>
-            {people.map((person) => {
+            {users.map((person) => {
                 const {id, name, age, image} = person
                 return (
-                    <div className="col">
-                        <div key={id} className="card" style={{width: '18rem'}}>
-                            <img src={image} className="card-img-top img-thumbnail" alt="..." />
+                    <div key={id} className='col-lg-6 card-position'>
+                        <div className="card" style={{width: "18rem"}}>
+                            <img className="card-img-top" src={image} alt={name} />
                             <div className="card-body">
                                 <h5 className="card-title">{name}</h5>
-                                <h5 className="card-title">{age} years</h5>
+                                <h5 className="card-title">Age: {age}</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <button className='btn btn-outline-primary' onClick={() => removeUser(id)}>Remove User</button>
                             </div>
                         </div>
                     </div>
